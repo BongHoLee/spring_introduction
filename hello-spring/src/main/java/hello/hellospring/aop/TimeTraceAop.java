346292bcdcf9ca61fa5655ca5715a4a4a19117f0
leebongho <bh9204@naver.com>
2021-08-24 23:06:06 +0900

    - 강의 4일차 AOP 간단 맛보깅

---
+++ hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,27 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    // 해당 패키지 하위에는 다 적용햇
+    // 메서드 호출될 때 마다 이 execute가 인터셉트에 ㅌ딱딱딱
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("Start : " + joinPoint.toString());
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("End : " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
