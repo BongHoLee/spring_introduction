923c42b5a43d9ad0aacfe689cbbfe3dca01e5161
leebongho <bh9204@naver.com>
2021-10-18 22:42:39 +0900

    Http Request Scope 테스트 - ObjectProvider 활용하여 요청

---
+++ userorder/src/main/java/userorder/userorder/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package userorder.userorder.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import userorder.userorder.common.MyLogger;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+
+}
