d1758aef071ca7d89da695ac2d13967248d6e350
leebongho <bh9204@naver.com>
2021-11-01 22:05:22 +0900

    기본 servlet 구현

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
+@ServletComponentScan   // 현재 패키지부터 하위 패키지까지 스캔하여 "서블릿을 찾아서 등록"한다.
 @SpringBootApplication
 public class ServletApplication {
 
