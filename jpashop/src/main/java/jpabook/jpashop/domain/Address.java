08a4cf45fc60ea450ebf62147041ba7ff62ac1c8
leebongho <bh9204@naver.com>
2022-01-12 23:32:54 +0900

    엔티티 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,8 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Delivery delivery;          // 배송 정보
+
+    private LocalDateTime orderDate;    // 주문 시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;     // 주문 상태 [ORDER, CANCEL]
+}
